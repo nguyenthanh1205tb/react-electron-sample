aa197112b43aa9757529c91b866e7d7f99451c64
nguyenthanh1205tb <nguyenthanh1205tb@gmail.com>
2022-03-31 15:25:45 +0700

    update data content

--- src/pages/installation/index.tsx
+++ src/pages/installation/index.tsx
@@ -8,14 +8,30 @@ function Installation({}: PropsWithChildren<InstallationProps>) {
       <div className="flex flex-row h-full">
         <div className="basis-2/5 p-4">
           <ul className="steps steps-vertical">
-            <li className="step step-primary">Install</li>
-            <li className="step step-primary">Software license Agreement</li>
-            <li className="step">Installing Setting</li>
-            <li className="step">Preinstallation Summary</li>
-            <li className="step">Preinstallation Summary</li>
-            <li className="step">Installing</li>
-            <li className="step">Installation Complete</li>
-            <li className="step">Finalize Settings</li>
+            <li data-content="✓" className="step step-primary">
+              Install
+            </li>
+            <li data-content="✕" className="step step-primary step-error">
+              Software license Agreement
+            </li>
+            <li data-content="●" className="step">
+              Installing Setting
+            </li>
+            <li data-content="●" className="step">
+              Preinstallation Summary
+            </li>
+            <li data-content="●" className="step">
+              Preinstallation Summary
+            </li>
+            <li data-content="●" className="step">
+              Installing
+            </li>
+            <li data-content="●" className="step">
+              Installation Complete
+            </li>
+            <li data-content="●" className="step">
+              Finalize Settings
+            </li>
           </ul>
         </div>
         <div className="basis-3/5 bg-white p-4"></div>
