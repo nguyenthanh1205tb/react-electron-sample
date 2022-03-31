560bf060c667e266879a31dff4a399b1e16d8492
nguyenthanh1205tb <nguyenthanh1205tb@gmail.com>
2022-03-31 12:58:51 +0700

    update script

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import * as ReactDOMClient from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import 'antd/dist/antd.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+const container = document.getElementById('root')
+
+if (container) {
+  const root = ReactDOMClient.createRoot(container)
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/installation/index.tsx
+++ src/pages/installation/index.tsx
@@ -3,9 +3,11 @@ import React, { PropsWithChildren } from 'react'
 interface InstallationProps {}
 function Installation({}: PropsWithChildren<InstallationProps>) {
   return (
-    <div className="flex flex-row">
-      <div className="basis-1/3 bg-red-300">left</div>
-      <div className="basis-2/3 bg-white">right</div>
+    <div className="h-screen w-full">
+      <div className="flex flex-row h-full">
+        <div className="basis-1/3 bg-red-300 ">left</div>
+        <div className="basis-2/3 bg-white">right</div>
+      </div>
     </div>
   )
 }
